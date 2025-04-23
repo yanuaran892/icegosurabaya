7757f4f5f3000244f51d4ce7dfaa4bd1a4e3b7c7
yanuarn892 <yanuaran8@gmail.com>
2025-04-23 18:51:40 +0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 // Pages
 import HomePage from './pages/HomePage';
